94ab0d3c57e45038908030af323ac08ffe90c7ab
leonardo benavides <leorbenavidesm@gmail.com>
2022-08-09 13:33:05 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { ref } from 'vue';
+import SizeSelector from './components/SizeSelector.vue';
+
+const selectedSize= ref("")
+</script>
+
+<template> 
+<div class="main">
+  <h1>seleccione una talla: {{selectedSize}}</h1>  
+  <div>
+    <SizeSelector
+      :selectedSize="selectedSize"
+      @on-change-size="selectedSize = $event"
+    />
+  </div>
+</div>
+</template>
+
+<style>
+  .main{
+    flex-direction: column;
+  }
+
+  .active{
+    background-color: #363636 !important;
+    color: white;
+    transition: all .3s ease-out;
+  }
+</style>
---
+++ src/components/SizeSelector.vue
@@ -0,0 +1,39 @@
+<script setup>
+const possibleSizes= ['XS','S','M','L','XL','XXL']
+const props= defineProps({
+    selectedSize:{required:true}
+})
+const emits= defineEmits({
+    onChangeSize: "",
+})
+</script>
+
+<template>
+<div class="main">
+    <h1>{{props.selectedSize}}</h1>
+    <div>
+        <button 
+        v-for="(size,i) in possibleSizes" 
+        :key=i
+        :class="{'active': props.selectedSize === size}"
+        @click="emits('onChangeSize', size)"
+        >
+        {{size}}
+        </button>
+    </div>
+</div>
+</template>
+<style scoped>
+    button{
+        background-color: #dad8d8;
+    }
+    .main{
+        display: flex;
+        flex-direction: column;
+    }
+    div{
+        display: flex;
+        gap: 5px;
+        justify-content: center;
+    }
+</style>
